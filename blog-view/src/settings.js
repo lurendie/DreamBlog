bd7578b30f9606247b60153d1da97cfcbc0a4362
lurendie <lurendie@gmail.com>
2026-01-31 11:27:18 +0800

    Add home backgrounds and update settings

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -3,9 +3,9 @@ module.exports = {
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1: 'https://cdn.naccl.top/blog/img/bg1.jpg',
-	bg2: 'https://cdn.naccl.top/blog/img/bg2.jpg',
-	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
+	bg1: './img/bg1.webp',
+	bg2: './img/bg2.webp',
+	bg3: '.img/bg3.webp',
 
 	/**
 	 * @type {string}
