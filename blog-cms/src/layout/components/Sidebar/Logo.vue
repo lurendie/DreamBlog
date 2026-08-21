b1fcdba1c9c4ea2b94e1a662ca41bf94b75e0c54
lurendie <lurendie@gmail.com>
2026-08-21 21:22:10 +0800

    fix: keep sidebar logo title visible

--- blog-cms/src/layout/components/Sidebar/Logo.vue
+++ blog-cms/src/layout/components/Sidebar/Logo.vue
@@ -1,15 +1,10 @@
 <template>
 	<div class="sidebar-logo-container" :class="{'collapse':collapse}">
-		<transition name="sidebarLogoFade">
-			<router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-				<img v-if="logo" :src="logo" class="sidebar-logo">
-				<h1 v-else class="sidebar-title">{{ title }} </h1>
-			</router-link>
-			<router-link v-else key="expand" class="sidebar-logo-link" to="/">
-				<img v-if="logo" :src="logo" class="sidebar-logo">
-				<h1 class="sidebar-title">{{ title }} </h1>
-			</router-link>
-		</transition>
+		<router-link class="sidebar-logo-link" to="/">
+			<img v-if="logo" :src="logo" class="sidebar-logo">
+			<h1 v-if="!collapse" class="sidebar-title">{{ title }}</h1>
+			<h1 v-else-if="!logo" class="sidebar-title">{{ title }}</h1>
+		</router-link>
 	</div>
 </template>
 
@@ -32,53 +27,49 @@
 </script>
 
 <style lang="scss" scoped>
-	.sidebarLogoFade-enter-active {
-		transition: opacity 1.5s;
-	}
-
-	.sidebarLogoFade-enter,
-	.sidebarLogoFade-leave-to {
-		opacity: 0;
-	}
-
 	.sidebar-logo-container {
 		position: relative;
 		width: 100%;
 		height: 58px;
-		line-height: 58px;
 		background: rgba(255, 255, 255, 0.92);
 		border-bottom: 1px solid rgba(148, 163, 184, 0.18);
-		text-align: left;
 		overflow: hidden;
 
 		& .sidebar-logo-link {
 			height: 100%;
 			width: 100%;
+			display: flex;
+			align-items: center;
+			text-decoration: none;
 
 			& .sidebar-logo {
 				width: 32px;
 				height: 32px;
-				vertical-align: middle;
+				flex: 0 0 32px;
 				margin: 0 12px 0 18px;
 				border-radius: 8px;
 			}
 
 			& .sidebar-title {
-				display: inline-block;
+				min-width: 0;
 				margin: 0;
 				color: #172033;
 				font-weight: 750;
-				line-height: 58px;
 				font-size: 16px;
 				font-family: Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
-				vertical-align: middle;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				white-space: nowrap;
 			}
 		}
 
 		&.collapse {
+			.sidebar-logo-link {
+				justify-content: center;
+			}
+
 			.sidebar-logo {
-				margin-right: 0px;
-				margin-left: 11px;
+				margin: 0;
 			}
 		}
 	}
